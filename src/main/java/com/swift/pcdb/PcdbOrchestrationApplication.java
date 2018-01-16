b9235a3661941b668ca9dd54ed65044f03ef72b8
Santosh Bharatha <santosh.bharatha@gmail.com>
2018-01-15 21:53:20 -0500

    initial commit

---
+++ src/main/java/com/swift/pcdb/PcdbOrchestrationApplication.java
@@ -0,0 +1,12 @@
+package com.swift.pcdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcdbOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcdbOrchestrationApplication.class, args);
+	}
+}
